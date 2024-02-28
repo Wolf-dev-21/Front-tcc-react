0b43aec00fb1ef73d62d87bf6de2a4df68b20bda
Wolf-dev-21 <wolf.of.art21@gmail.com>
2024-02-27 22:23:31 -0300

    Login com bootstrap

--- front-tcc/src/App.js
+++ front-tcc/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import { Button, } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{justifyContent: 'center', width:'30%'}}>
+      <h1>Login</h1>
+
+      <Form>
+        <Form.Group className="mt-3" as={Row}>
+          <Form.Label collum sm="2">Email</Form.Label>
+            <Col sm='10'>
+              <Form.Control type='email' placeholder='Digite email'/>
+            </Col>
+        </Form.Group>
+
+        <Form.Group className="mt-3" as={Row}>
+          <Form.Label collum sm="2">Senha</Form.Label>
+            <Col sm='10'>
+              <Form.Control type='password' placeholder='Digite email'/>
+            </Col>
+        </Form.Group>
+
+        <Form.Group className="mt-3" as={Row}>
+          <Button variant="primary" type="">Log-in</Button>
+        </Form.Group>
+
+      </Form>
+
+    </Container>
   );
 }
 
